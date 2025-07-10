bd5d94d75528b34339fcbfd8b120c2b4f5c2a90b
Thimo <info@thimo.org>
2025-07-10 10:30:43 +0200

    first commit

---
+++ src/app/scoreboard-page-component/scoreboard-page-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scoreboard-page-component',
+  imports: [],
+  templateUrl: './scoreboard-page-component.html',
+  styleUrl: './scoreboard-page-component.css'
+})
+export class ScoreboardPageComponent {
+
+}
