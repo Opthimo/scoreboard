91c6e4341993a11c7b941a40bc0819638958b2b2
Thimo <info@thimo.org>
2025-07-10 10:39:28 +0200

    added ScoreboardPageComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScoreboardPageComponent } from './scoreboard-page-component/scoreboard-page-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScoreboardPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
