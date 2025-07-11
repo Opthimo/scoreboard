f405f773ff26a4fc618c759a3a1d9acbcbeeab23
Thimo <info@thimo.org>
2025-07-11 12:55:27 +0200

    added a tailwind table in scoreboard-page-component.html enabled ControlFlow in tsconfig ("enableControlFlow": true) for using @for added a for-loop to list and sort the players in the table.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ScoreboardPageComponent } from './scoreboard-page-component/scoreboard-page-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ScoreboardPageComponent],
+  imports: [ScoreboardPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/scoreboard-page-component/scoreboard-page-component.ts
+++ src/app/scoreboard-page-component/scoreboard-page-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Player } from '../models/player';
 
 @Component({
   selector: 'app-scoreboard-page',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './scoreboard-page-component.css'
 })
 export class ScoreboardPageComponent {
+players: Player[] = [
+    { id: 1, name: 'Alice', score: 10 },
+    { id: 2, name: 'Bob', score: 15 },
+    { id: 3, name: 'Charlie', score: 5 },
+    { id: 4, name: 'Diana', score: 20 }
+  ];
 
+trackByPlayerId(index: number, player: Player): number {
+  return player.id;
+}
 }
