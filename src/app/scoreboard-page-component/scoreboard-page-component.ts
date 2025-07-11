1fcc1d22ec6f5a0b117e692fc6470ef04a96b8ce
Thimo <info@thimo.org>
2025-07-11 12:32:54 +0200

    renamed app-scoreboard-page-component to app-scoreboard-page

--- src/app/scoreboard-page-component/scoreboard-page-component.ts
+++ src/app/scoreboard-page-component/scoreboard-page-component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-scoreboard-page-component',
+  selector: 'app-scoreboard-page',
   imports: [],
   templateUrl: './scoreboard-page-component.html',
   styleUrl: './scoreboard-page-component.css'
